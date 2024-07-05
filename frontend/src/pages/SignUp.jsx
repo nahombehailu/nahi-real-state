9092c77dc41164e53b292916baefeb6aada08ad3
nahombehailu <nahbehailu@gmail.com>
2024-07-05 17:05:21 +0300

    complete signup functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
 
 export default function SignUp() {
   const [formData,setFormData]=useState({});
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(null);
+  const navigate=useNavigate()
+
+
 
 
   const handleChange=(e)=>{
@@ -12,6 +18,7 @@ export default function SignUp() {
   const handleSubmit=async(e)=>{
     e.preventDefault();
     try {
+      setLoading(true)
       const res=await fetch('/api/auth/signup',{
         method:"POST",
         headers:{"Content-Type":"application/json"},
@@ -19,10 +26,19 @@ export default function SignUp() {
       })
   
       const data= await res.json()
+      if(data.success===false){
+        setError(data.message)
+        setLoading(false)
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/signin')
       console.log(data);
       
     } catch (error) {
+      setLoading(false)
       console.log(error.message);
+
     }
 
 
@@ -39,8 +55,13 @@ export default function SignUp() {
     <input type="text" id='email' onChange={handleChange} /><br />
     <label htmlFor="password">password</label><br />
     <input type="text" id='password' onChange={handleChange} /><br />
-    <button className='text-red-300 rounded bg-green-300'>signup</button>
+    <button disabled={loading} className='text-red-300 rounded bg-green-300'>
+      {loading? loading :'signup'}</button>
    </form>
+   <div>{error &&(
+    <p className='text-red-700'>{error}</p>
+   )}
+   </div>
     </div>
   )
 }
