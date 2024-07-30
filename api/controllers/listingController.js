e47496590f5ce13bff7e37c9becd4bce853fa1bd
nahombehailu <nahbehailu@gmail.com>
2024-07-30 21:15:41 +0300

    complete contact pages adn create serach api

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -71,3 +71,48 @@ export const getListing=async(req,res,next)=>{
         next(error)
     }
 }
+
+export const getListings =async (req,res,next)=>{
+    try {
+       const limit=parseInt(req.query.limit) || 9;
+       const startIndex=parseInt(req.query.startIndex) || 0;
+       let offer=req.query.offer;
+       if(offer==='false' || offer===undefined){
+        offer={$in:[false,true]}
+       }
+
+       let parking=req.query.parking;
+       if(parking==='false' || parking===undefined){
+        {$in:[false,true]}
+       }
+
+       let furnished=req.query.furnished;
+       if(furnished==='false' || furnished===undefined){
+        furnished={$in:[false,true]}
+       }
+        let type=req.query.type;
+       if(type===undefined || type==='all'){
+        type={$in:['sale','rent']}
+       }
+
+       const searchTerm=req.query.searchTerm || ""
+        const sort=req.query.sort || "createdAt"
+        const order=req.query.order || "desc"
+
+       const listings=await Listing.find({
+        name:{$regex:searchTerm,$options:"i"},
+        offer,
+        parking,
+        furnished,
+        type,
+       }).sort(
+        {[sort]:order}
+       ).limit(limit).skip(startIndex)
+     
+      return res.status(200).json(listings);
+    } catch (error) {
+       next(error)
+    }
+      
+      }
+   
